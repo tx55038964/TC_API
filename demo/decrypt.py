baafa6df493b01c8cdff1de88429d344d2d51520
wangxu <7774381@qq.com>
2023-07-06 19:56:57 +0800

    :sparkles: feat: 更新代码。

--- demo/decrypt.py
+++ demo/decrypt.py
@@ -1,19 +1,19 @@
 from util.EncryptData import EncryptData
 
-# 定义json路径
-path = r"../json/response.txt"
+# ??json??
+path = r"../json/response.json"
 file = open(path, 'r', encoding='utf-8')
 
-file_result = open(r"../json/data.json", 'a')
+file_result = open(r"../json/data.json", 'a', encoding='utf-8')
 
-# 秘钥
-password = 'DJTDLRH3R7M9BINRL5O2E6O7MJM8LUXB'
+# ??
+password = 'MQ0WINT60DXP7U7R09A70V6Z1SEDIGYH'
 
-# 需要加密的内容, file.read() 读取文件内容
+# ???????, file.read() ??????
 data = file.read()
-# 这里**的长度必须是16的倍数
+# ??**??????16???
 eg = EncryptData(password)
 res = eg.decrypt(str(data))
 
-# 写入到file_result文件中的内容, file_result.write() 写入文件内容
+# ???file_result??????, file_result.write() ??????
 file_result.write(res)
